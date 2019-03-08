a297b8cdc44090c2d3bbae97c1e7006d49aeed42
BinkerKing <15000388149@163.com>
2019-03-09 02:14:17 +0800

    新增笔记页面

--- infinite-web/src/main/resources/static/core/js/table.js
+++ infinite-web/src/main/resources/static/core/js/table.js
@@ -2,10 +2,7 @@
 //通过分页方式，选择加载数据的方式
 //通过
 var TableUtil = {
-	init : function (){
-
-	},
-	init1 : function (queryUrl) {
+	init : function (queryUrl) {
         var table = $('#ArbetTable').bootstrapTable({
             url: queryUrl,         //请求后台的URL（*）
             method: 'get',                      //请求方式（*）
@@ -19,14 +16,14 @@ var TableUtil = {
             pageNumber: 1,                       //初始化加载第一页，默认第一页
             pageSize: 10,                       //每页的记录行数（*）
             pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
-            search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
             contentType: "application/x-www-form-urlencoded",
             strictSearch: true,
             showColumns: true,                  //是否显示所有的列
             showRefresh: true,                  //是否显示刷新按钮
             minimumCountColumns: 2,             //最少允许的列数
             clickToSelect: true,                //是否启用点击选中行
-            height: $(window).height()*0.79,    //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            //height: $(window).height()*0.79,    //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
             uniqueId: "id",                     //每一行的唯一标识，一般为主键列
             showToggle: false,                    //是否显示详细视图和列表视图的切换按钮
             cardView: false,                    //是否显示详细视图
@@ -34,14 +31,13 @@ var TableUtil = {
             columns: [
                 {
                     field: 'id',
-                    title: 'ID',
-                    sortable: true
+                    title: '模版名称'
                 }, {
                     field: 'name',
-                    title: '名字'
+                    title: '类型'
                 }, {
                     field: 'sex',
-                    title: '性别'
+                    title: '描述'
                 },
                 {
                     field: 'operate',
@@ -74,10 +70,10 @@ var TableUtil = {
 	}
 }
 function operateFormatter(value, row, index) {//赋予的参数
-    return [
-        '<a class="btn active disabled" href="#">编辑</a>',
-        '<a class="btn active" href="#">档案</a>',
-        '<a class="btn active" href="#">记录</a>',
-        '<a class="btn active" href="#">准入</a>'
-    ].join('');
+    // return [
+    //     '<a class="btn active disabled" href="#">编辑</a>',
+    //     '<a class="btn active" href="#">档案</a>',
+    //     '<a class="btn active" href="#">记录</a>',
+    //     '<a class="btn active" href="#">准入</a>'
+    // ].join('');
 }
